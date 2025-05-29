48a3160a842a433462d7704e8196a4afe312b348
edenilson36 <cr241530@alumno.udb.edu.sv>
2025-05-29 13:17:36 -0600

    Creacion de paquete dto y config, como añadir nuevas clases

---
+++ src/main/java/sv/edu/udb/dto/RegisterRequest.java
@@ -0,0 +1,12 @@
+package sv.edu.udb.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private Integer age;
+}
